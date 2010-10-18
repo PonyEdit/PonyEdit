f83c702d87e955521934b250a1349c1729efa9bf
Mark George <mark@thingalon.com>
2010-10-18 01:20:02 +0000

    Tidy up, icons on top level of directory tree

--- filedialog.cpp
+++ filedialog.cpp
@@ -1,6 +1,6 @@
 #include "filedialog.h"
 #include "ui_filedialog.h"
-#include <QFileIconProvider>
+
 #include <QDebug>
 #include <QDir>
 
@@ -9,43 +9,69 @@ FileDialog::FileDialog(QWidget *parent) :
     ui(new Ui::FileDialog)
 {
 	ui->setupUi(this);
+	populateFolderTree();
 
-	QFileIconProvider p;
-
-	QTreeWidgetItem* local = new QTreeWidgetItem(QStringList("Local Computer"), 0);
-	ui->directoryTree->addTopLevelItem(local);
-
-	QString homePath = QDir::homePath();
-	QFileInfo fi(homePath);
-
-	QTreeWidgetItem* child = new QTreeWidgetItem(QStringList(fi.baseName()), 0);
-	child->setIcon(0, p.icon(fi));
-	local->addChild(child);
-
-	QFileInfoList list = QDir::drives();
-	foreach (QFileInfo dfi, list)
-	{
-		QString driveName = dfi.absolutePath();
-		driveName.replace('/', '\\');
-		child = new QTreeWidgetItem(QStringList(driveName), 0);
-
-		child->setIcon(0, p.icon(dfi));
-		local->addChild(child);
-	}
-
-	QTreeWidgetItem* remote = new QTreeWidgetItem(QStringList("Remote Servers"), 0);
-	ui->directoryTree->addTopLevelItem(remote);
-
-	QTreeWidgetItem* favourites = new QTreeWidgetItem(QStringList("Favourite Locations"), 0);
-	ui->directoryTree->addTopLevelItem(favourites);
-
-	QList<int> sizes;
-	sizes.append(30);
-	sizes.append(70);
-	ui->splitter->setSizes(sizes);
+	connect(ui->directoryTree, SIGNAL(itemExpanded(QTreeWidgetItem*)), this, SLOT(folderTreeItemExpanded(QTreeWidgetItem*)));
 }
 
 FileDialog::~FileDialog()
 {
     delete ui;
 }
+
+void FileDialog::addLocalFile(const QString& label, const QFileInfo& fileInfo, QTreeWidgetItem* parent)
+{
+	QString realLabel = label;
+	#ifdef Q_WS_WIN
+		realLabel.replace('/', '\\');
+	#endif
+
+	QTreeWidgetItem* newItem = new QTreeWidgetItem(QStringList(label), 0);
+	newItem->setIcon(0, mIconProvider.icon(fileInfo));
+	if (fileInfo.isDir())
+		newItem->setChildIndicatorPolicy(QTreeWidgetItem::ShowIndicator);
+
+	parent->addChild(newItem);
+}
+
+void FileDialog::populateFolderTree()
+{
+	//
+	//	Local computer; contains home dir and root path(s)
+	//
+
+	QTreeWidgetItem* localComputer = new QTreeWidgetItem(QStringList("Local Computer"), 0);
+	localComputer->setIcon(0, mIconProvider.icon(QFileIconProvider::Computer));
+	ui->directoryTree->addTopLevelItem(localComputer);
+
+	QFileInfo homeFolder(QDir::homePath());
+	addLocalFile(homeFolder, localComputer);
+
+	QFileInfoList driveList = QDir::drives();
+	foreach (QFileInfo driveFileInfo, driveList)
+		addLocalFile(driveFileInfo.filePath(), driveFileInfo, localComputer);
+
+	//
+	//	Remote Servers; contains a list of pre-configured known servers
+	//
+
+	QTreeWidgetItem* remoteServers = new QTreeWidgetItem(QStringList("Remote Servers"), 0);
+	remoteServers->setIcon(0, mIconProvider.icon(QFileIconProvider::Network));
+	ui->directoryTree->addTopLevelItem(remoteServers);
+
+	//
+	//	Favorite Locations; contains a list of bookmarked locations; local or otherwise
+	//
+
+	QTreeWidgetItem* favouriteLocations = new QTreeWidgetItem(QStringList("Favorite Locations"), 0);
+	favouriteLocations->setIcon(0, QIcon("icons/favorite.png"));
+	ui->directoryTree->addTopLevelItem(favouriteLocations);
+}
+
+void FileDialog::folderTreeItemExpanded(QTreeWidgetItem* item)
+{
+
+}
+
+
+
--- filedialog.h
+++ filedialog.h
@@ -2,6 +2,8 @@
 #define FILEDIALOG_H
 
 #include <QDialog>
+#include <QFileIconProvider>
+#include <QTreeWidgetItem>
 
 namespace Ui {
     class FileDialog;
@@ -10,13 +12,20 @@ namespace Ui {
 class FileDialog : public QDialog
 {
     Q_OBJECT
-
 public:
     explicit FileDialog(QWidget *parent = 0);
     ~FileDialog();
 
+private slots:
+	void folderTreeItemExpanded(QTreeWidgetItem* item);
+
 private:
+	void populateFolderTree();
+	void addLocalFile(const QString& label, const QFileInfo& fileInfo, QTreeWidgetItem* parent);
+	void addLocalFile(const QFileInfo& fileInfo, QTreeWidgetItem* parent) { addLocalFile(fileInfo.fileName(), fileInfo, parent); }
+
     Ui::FileDialog *ui;
+	QFileIconProvider mIconProvider;
 };
 
 #endif // FILEDIALOG_H
