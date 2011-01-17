cd48e73db3ddfa420b85d80984485254c583f614
Gary Pendergast <gary@pento.net>
2011-01-17 21:46:29 +0000

    Upped version to 0.9-prealpha2

--- main/main.cpp
+++ main/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
 		QCoreApplication::setOrganizationName("Pentalon");
 		QCoreApplication::setApplicationName("PonyEdit");
-		QCoreApplication::setApplicationVersion("0.9-prealpha1");
+		QCoreApplication::setApplicationVersion("0.9-prealpha2");
 		gDispatcher = new GlobalDispatcher();
 
 		SshConnection::initializeLib();
