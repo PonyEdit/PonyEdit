8dc49c64b3cef01365545fa39000f02c78625140
Mark George <mark@thingalon.com>
2011-03-21 08:08:30 +0000

    Why didn't I just use indexOf rather than manually iterating through the list?

--- file/openfiletreemodel.cpp
+++ file/openfiletreemodel.cpp
@@ -81,9 +81,7 @@ QModelIndex OpenFileTreeModel::getNodeIndex(Node* node) const
 {
 	Node* parentNode = node->parent;
 	if (!parentNode) return QModelIndex();
-	int row = 0;
-	for (; row < parentNode->children.length() && parentNode->children[row] != node; row++) ;
-	return createIndex(row, 0, static_cast<void*>(node));
+        return createIndex(parentNode->children.indexOf(node), 0, static_cast<void*>(node));
 }
 
 void OpenFileTreeModel::addNodeToTree(Node* parentNode, Node* node)
