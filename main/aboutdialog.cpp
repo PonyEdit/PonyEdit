08f0a054e994c749859aa44d0ea08067fbe3649d
Gary Pendergast <gary@pento.net>
2011-06-07 03:38:31 +0000

    Add licence details to the About box

--- main/aboutdialog.cpp
+++ main/aboutdialog.cpp
@@ -1,5 +1,7 @@
 #include <QCoreApplication>
 
+#include "licence/licence.h"
+
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
@@ -10,6 +12,35 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
 	ui->versionLabel->setText(ui->versionLabel->text().arg(QCoreApplication::applicationVersion()).arg(MAJOR_VERSION).arg(MINOR_VERSION).arg(REVISION));
+
+	Licence l = Licence();
+
+	QString login = l.getLogin();
+	if(login.isEmpty())
+		login = tr("Anonymous");
+
+	QString details;
+	if(!l.getExpiry().isNull())
+		details = tr("Expires: %1").arg(l.getExpiry().toString(Qt::ISODate));
+	else
+		details = tr("Expires: (No Expiry)");
+
+	details += "<br />";
+
+	int version = l.getMaximumVersion();
+	if(version <= 0)
+		details += tr("Max Version: (No Maximum Version)");
+	else
+		details += tr("Max Version: %1").arg(version);
+
+	details += "<br/>";
+
+	details += tr("Key: ") + "<br/>";
+	details += l.getKey();
+
+	ui->licenceLabel->setText(ui->licenceLabel->text().arg(login));
+	ui->licenceDetails->appendHtml(details);
+	ui->licenceDetails->moveCursor(QTextCursor::Start);
 }
 
 AboutDialog::~AboutDialog()
