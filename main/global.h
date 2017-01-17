3409fcc2d40f25b0432436f8f6560f6be30cd474
Gary Pendergast <gary@pento.net>
2017-01-17 20:50:50 +1100

    clang doesn't understand the -Wterminate compiler option

--- main/global.h
+++ main/global.h
@@ -11,10 +11,18 @@
 //#define LOCK_MUTEX(x) { //qDebug() << "Locking mutex " #x " at line " << __LINE__ << " in file " << __FILE__ << " in object " << (void*)this << ", thread " << (void*)QThread::currentThread(); x.lock(); }
 //#define UNLOCK_MUTEX(x) { //qDebug() << "Unlocking mutex " #x " at line " << __LINE__ << " in file " << __FILE__ << " in object " << (void*)this << ", thread " << (void*)QThread::currentThread(); x.unlock(); }
 
+
+// clang doesn't understand -Wterminate
+#ifdef __clang__
+#define HIDE_COMPILE_WARNINGS _Pragma("GCC diagnostic push") \
+	_Pragma("GCC diagnostic ignored \"-Weffc++\"") \
+	_Pragma("GCC diagnostic ignored \"-Wnon-virtual-dtor\"")
+#else
 #define HIDE_COMPILE_WARNINGS _Pragma("GCC diagnostic push") \
 	_Pragma("GCC diagnostic ignored \"-Weffc++\"") \
 	_Pragma("GCC diagnostic ignored \"-Wnon-virtual-dtor\"") \
 	_Pragma("GCC diagnostic ignored \"-Wterminate\"")
+#endif
 
 #define UNHIDE_COMPILE_WARNINGS _Pragma("GCC diagnostic pop")
 
